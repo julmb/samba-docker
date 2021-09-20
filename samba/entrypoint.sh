5bf7381e98d8757ad657f315d8520d8b65ea0c82
Julian Brunner <julianbrunner@gmail.com>
2021-09-20 20:49:50 +0200

    new entrypoint design

--- samba/entrypoint.sh
+++ samba/entrypoint.sh
@@ -9,9 +9,31 @@ while read line
 do
     username=$(echo $line | cut -d " " -f 1)
     password=$(echo $line | cut -d " " -f 2)
-    echo "adding $username..."
+
+    echo "adding $username with password $password..."
     adduser --disabled-password --no-create-home $username
     printf "$password\n$password" | smbpasswd -s -a $username
-done < $1
+done < /etc/samba/users.conf
+
+echo "[global]" > /etc/samba/smb.conf
+echo "    log file = /var/log/samba/%m" >> /etc/samba/smb.conf
+echo "    log level = 1" >> /etc/samba/smb.conf
+echo "    server role = standalone server" >> /etc/samba/smb.conf
+
+echo >> /etc/samba/smb.conf
+
+while read line
+do
+    name=$(echo $line | cut -d " " -f 1)
+    path=$(echo $line | cut -d " " -f 2)
+	users=$(echo $line | cut -d " " -f 3-)
+
+	echo "adding share $name at $path for users [$users]"
+	echo "[$name]" >> /etc/samba/smb.conf
+	echo "    path = $path" >> /etc/samba/smb.conf
+	echo "    read only = no" >> /etc/samba/smb.conf
+	echo "    valid users = $users" >> /etc/samba/smb.conf
+	echo "    inherit permissions = yes" >> /etc/samba/smb.conf
+done < /etc/samba/shares.conf
 
 exec "$@"
